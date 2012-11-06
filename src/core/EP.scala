a2caa4ff80f5a7ccd3852d4706b0d1b52bbf0a83
Roberto Rodrigues <me@rrodrigues.net>
2012-11-06 16:29:49 -0200

    ajuste dos arquivos de dados e escolha dos atributos e metodos da classe Dados

--- src/core/EP.scala
+++ src/core/EP.scala
@@ -3,3 +3,48 @@ package core
 class EP {
 
 }
+class NaiveBayes(dados: Dados) {
+
+}
+class Dados(pathNames: String, pathData: String) {
+
+  var frequenciaClasse = new Array[Double](qtClasses)
+  var frequenciaAtributo = new Array[Double](qtAtributos)
+  var probabilidadeDeUmaClasseOcorra = new Array[Double](qtClasses)
+  var probabilidadeDeUmAtributoOcorra = new Array[Double](qtAtributos)
+  var probabilidadeDeUmAtributoDadoUmaClasse = new Array[Array[Double]](qtAtributos).map(a => new Array[Double](qtClasses))
+  var probabilidadeDeUmClasseDadoUmAtributo = new Array[Array[Double]](qtClasses).map(a => new Array[Double](qtAtributos))
+  var qtClasses: Int = setQtCalsses
+  var qtAtributos: Int = setQtAtributos
+  setFrequenciaClasse
+  setFrequenciaAtributo
+  setProbabilidadeDeUmaClasseOcorra
+  setProbabilidadeDeUmAtributoOcorra
+  setProbabilidadeDeUmAtributoDadoUmaClasse
+  setProbabilidadeDeUmClasseDadoUmAtributo
+
+  def setQtCalsses() = {
+    0 // le do arquivo
+  }
+  def setQtAtributos() = {
+    0 // le do arquivo
+  }
+  def setFrequenciaClasse() = {
+
+  }
+  def setFrequenciaAtributo() = {
+
+  }
+  def setProbabilidadeDeUmaClasseOcorra() = {
+
+  }
+  def setProbabilidadeDeUmAtributoOcorra() = {
+
+  }
+  def setProbabilidadeDeUmAtributoDadoUmaClasse() = {
+
+  }
+  def setProbabilidadeDeUmClasseDadoUmAtributo() = {
+
+  }
+}
