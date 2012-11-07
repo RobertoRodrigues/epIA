71d81ebb9c08fc5b6f180e1296f6a0f79574d195
Roberto Rodrigues <me@rrodrigues.net>
2012-11-07 17:48:52 -0200

    feito o draft até o fim do passo 2

--- src/core/EP.scala
+++ src/core/EP.scala
@@ -8,10 +8,57 @@ import scala.collection.mutable.ArrayBuffer
 class EP {
 
 }
-class NaiveBayes(dados: Dados) {
+class CrossValidation(pathNames: String, pathData: String) {
+  val dados = new Dados(pathNames, pathData, 0.1)
+
+  val r = new NaiveBayes(new Dados(pathNames, pathData, 0.1)).taxaDeErro //10 interações guardando em um array
+  val errosMedios = new Array[Double](10)
+  val erroMedio = 10 // soma das 10 interacoes dos errosMedios dividido por 10 que é a media dos erros
+
+  val SE = math.sqrt((erroMedio * (1 - erroMedio)) / (dados.tamanhoDaBaseDeTeste + dados.tamanhoDaBase))
+  val intervaloDeConfiancaInferior = erroMedio - 1.96 * SE
+  val intervaloDeConfiancaSuperior = erroMedio + 1.96 * SE
 
 }
-class Dados(pathNames: String, pathData: String) {
+
+class NaiveBayes(dados: Dados) {
+
+  val mC = calculaMatrizConfusao
+
+  def probabilidadeDeOcorrer(valores: Array[Valores]) = {
+    var p1 = dados.probabilidadeDeUmaClasseOcorra(1) // * todas as probabilidades dos valores dado que a classe é 1
+    var p0 = dados.probabilidadeDeUmaClasseOcorra(0) // * todas as probabilidades dos valores dado que a classe é 0
+    p1 = p1 / (p1 + p0)
+  }
+  def calculaMatrizConfusao() = {
+    //fazer um for percorrendo o conjunto de teste e comparando a saida com a esperada devolvendo a matriz confusão 
+    //a matriz confusão é 2x2 registrando a quantidade de: saida 1 esperado 1, saida 1 esperado 0, saida 0 esperado 0
+    //e saida 0 esperado 1
+    //
+    //VP  FP
+    //FN VN
+    0
+  }
+  def taxaDeFalsoPositivo() = {
+    //mC(0)(1) / (mC(0)(1) + mC(1)(1))
+  }
+  def taxaDeFalsoNegativo() = {
+    //mC(1)(0) / (mC(1)(0) + mC(0)(0))
+  }
+  def taxaDeErro() = {
+    //(taxaDeFalsoNegativo + taxaDeFalsoPositivo) / (mC(0)(0) + mC(0)(1) + mC(1)(0) + mC(1)(1))
+  }
+  def acuracia() = {
+    //1-taxaDeErroTotal
+  }
+
+}
+class Valores {
+
+}
+
+class Dados(pathNames: String, pathData: String, pTreinamento: Double) {
+  //separar os dados em dois grupos de treinamento e teste sendo a divisão aleatória
 
   var str = ""
   val fileNames: BufferedReader = new BufferedReader(new FileReader(pathNames))
@@ -31,11 +78,13 @@ class Dados(pathNames: String, pathData: String) {
   }
   fileData.close();
 
-  val tamanhoDaBase = strData.size
+  val dadosDeTeste = new Array[Array[Valores]](qtAtributos + 1).map(a => new Array[Valores](tamanhoDaBaseDeTeste))
+  val tamanhoDaBaseDeTeste: Int = strData.size - tamanhoDaBase
+  val tamanhoDaBase = (strData.size * pTreinamento).toInt
   val frequenciaClasse = new Array[Int](qtClasses)
-  val frequenciaAtributo = new Array[Array[Int]](qtAtributos).map(a => new Array[Int](qtMaxVal)) //gostaria de colocar os valores dos atributos e suas frequencias
+  val frequenciaAtributo = new Array[Array[Valores]](qtAtributos).map(a => new Array[Int](qtMaxVal)) //gostaria de colocar os valores dos atributos e suas frequencias
   val probabilidadeDeUmaClasseOcorra = new Array[Double](qtClasses)
-  val probabilidadeDeUmAtributoDadoUmaClasse = new Array[Array[Double]](qtAtributos).map(a => new Array[Double](qtClasses))
+  val probabilidadeDeUmAtributoDadoUmaClasse = new Array[Array[Valores]](qtAtributos).map(a => new Array[Double](qtClasses))
   val qtClasses: Int = 2
   val qtAtributos: Int = strNames.size - 1
   val qtMaxVal = setQtMaxVal // preferia ter um por valor
--- test/core/TesteEp.scala
+++ test/core/TesteEp.scala
@@ -7,93 +7,99 @@ class testeEP {
 
   @Test
   def TamanhoDaBasePlaytennis() {
-    var ep = new Dados("data/playTennis.names", "data/playTennis.data");
+    var ep = new Dados("data/playTennis.names", "data/playTennis.data", 0);
     assertEquals(14, ep.tamanhoDaBase)
   }
   @Test
   def QuantidadeDeAtributosPlaytennis() {
-    var ep = new Dados("data/playTennis.names", "data/playTennis.data");
+    var ep = new Dados("data/playTennis.names", "data/playTennis.data", 0);
     assertEquals(5, ep.qtAtributos)
   }
   @Test
   def frequenciaClasse0Playtennis() {
-    var ep = new Dados("data/playTennis.names", "data/playTennis.data");
+    var ep = new Dados("data/playTennis.names", "data/playTennis.data", 0);
     assertEquals(4, ep.frequenciaClasse(0))
   }
   @Test
   def frequenciaClasse1Playtennis() {
-    var ep = new Dados("data/playTennis.names", "data/playTennis.data");
+    var ep = new Dados("data/playTennis.names", "data/playTennis.data", 0);
     assertEquals(10, ep.frequenciaClasse(1))
   }
   @Test
   def frequenciaAtributoTempoSolPlaytennis() {
-    var ep = new Dados("data/playTennis.names", "data/playTennis.data");
+    var ep = new Dados("data/playTennis.names", "data/playTennis.data", 0);
     //assertEquals(5, ep.frequenciaAtributo(tempo)(sol))
   }
   @Test
   def frequenciaAtributoTempoNubladoPlaytennis() {
-    var ep = new Dados("data/playTennis.names", "data/playTennis.data");
+    var ep = new Dados("data/playTennis.names", "data/playTennis.data", 0);
     //assertEquals(4, ep.frequenciaAtributo(tempo)(nublado))
   }
   @Test
   def frequenciaAtributoTempoChuvaPlaytennis() {
-    var ep = new Dados("data/playTennis.names", "data/playTennis.data");
+    var ep = new Dados("data/playTennis.names", "data/playTennis.data", 0);
     //assertEquals(5, ep.frequenciaAtributo(tempo)(chuva))
   }
   @Test
   def frequenciaAtributoTemperaturaQuentePlaytennis() {
-    var ep = new Dados("data/playTennis.names", "data/playTennis.data");
+    var ep = new Dados("data/playTennis.names", "data/playTennis.data", 0);
     //assertEquals(4, ep.frequenciaAtributo(temperatura)(quente))
   }
   @Test
   def frequenciaAtributoTemperaturaMedianaPlaytennis() {
-    var ep = new Dados("data/playTennis.names", "data/playTennis.data");
+    var ep = new Dados("data/playTennis.names", "data/playTennis.data", 0);
     //assertEquals(6, ep.frequenciaAtributo(temperatura)(mediana))
   }
   @Test
   def frequenciaAtributoTemperaturaFriaPlaytennis() {
-    var ep = new Dados("data/playTennis.names", "data/playTennis.data");
+    var ep = new Dados("data/playTennis.names", "data/playTennis.data", 0);
     //assertEquals(4, ep.frequenciaAtributo(temperatura)(frio))
   }
   @Test
   def frequenciaAtributoUmidadeAltaPlaytennis() {
-    var ep = new Dados("data/playTennis.names", "data/playTennis.data");
+    var ep = new Dados("data/playTennis.names", "data/playTennis.data", 0);
     //assertEquals(7, ep.frequenciaAtributo(umidade)(alta))
   }
   @Test
   def frequenciaAtributoUmidadeNormalPlaytennis() {
-    var ep = new Dados("data/playTennis.names", "data/playTennis.data");
+    var ep = new Dados("data/playTennis.names", "data/playTennis.data", 0);
     //assertEquals(7, ep.frequenciaAtributo(umidade)(normal))
   }
   @Test
   def frequenciaAtributoVentoFortePlaytennis() {
-    var ep = new Dados("data/playTennis.names", "data/playTennis.data");
+    var ep = new Dados("data/playTennis.names", "data/playTennis.data", 0);
     //assertEquals(6, ep.frequenciaAtributo(vento)(forte))
   }
   @Test
   def frequenciaAtributoVentoFracoPlaytennis() {
-    var ep = new Dados("data/playTennis.names", "data/playTennis.data");
+    var ep = new Dados("data/playTennis.names", "data/playTennis.data", 0);
     //assertEquals(8, ep.frequenciaAtributo(vento)(fraco))
   }
   @Test
   def probabilidadeClasse0Playtennis() {
-    var ep = new Dados("data/playTennis.names", "data/playTennis.data");
+    var ep = new Dados("data/playTennis.names", "data/playTennis.data", 0);
     assertEquals(5 / 14, ep.probabilidadeDeUmaClasseOcorra(0), math.E)
   }
   @Test
   def probabilidadeClasse1Playtennis() {
-    var ep = new Dados("data/playTennis.names", "data/playTennis.data");
+    var ep = new Dados("data/playTennis.names", "data/playTennis.data", 0);
     assertEquals(9 / 14, ep.probabilidadeDeUmaClasseOcorra(1), math.E)
   }
   @Test
   def probabilidadeDeVentoForteDadoClasse1Playtennis() {
-    var ep = new Dados("data/playTennis.names", "data/playTennis.data");
+    var ep = new Dados("data/playTennis.names", "data/playTennis.data", 0);
     //assertEquals(3 / 9, ep.probabilidadeDeUmAtributoDadoUmaClasse(vento)(forte)(1), math.E)
   }
   @Test
   def probabilidadeDeVentoForteDadoClasse0Playtennis() {
-    var ep = new Dados("data/playTennis.names", "data/playTennis.data");
+    var ep = new Dados("data/playTennis.names", "data/playTennis.data", 0);
     //assertEquals(3 / 5, ep.probabilidadeDeUmAtributoDadoUmaClasse(vento)(forte)(0), math.E)
+  }
+  @Test
+  def probabilidadeDeOcorrerDadoSolMedianaAltaFortePlaytennis() {
+    var ep = new Dados("data/playTennis.names", "data/playTennis.data", 0);
+    var naiveBayes = new NaiveBayes(ep)
+    //assertEquals(0.205, naiveBayes.probabilidadeDeOcorrer((sol, mediana, alta, forte)), math.E)
   }
 
 }
